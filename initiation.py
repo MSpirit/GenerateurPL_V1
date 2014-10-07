5978aa421dd281c3ab510e4ea2c70dd4d4e5d7b5
Romain Locquet <romain.locquet@bts-malraux.net>
2014-10-07 14:42:35 +0000

    Version fonctionnelle du programme datant du 07/10/14

--- initiation.py
+++ initiation.py
@@ -1,5 +1,10 @@
-import argparse
 import logging
+import argparse
+import sys
+
+logging.basicConfig(filename ="Journal_log.log", level = logging.DEBUG)
+logging.info("Mise en marche du programme")
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("dureePlaylist", type = int, help="duree de la playlist en minutes")
@@ -7,28 +12,46 @@ parser.add_argument("formatPlaylist", choices = ["m3u","xspf","pls"], help="form
 parser.add_argument("nomFichierPlaylist", help="nom du fichier de sortie")
 
 
-parser.add_argument("--titrePlaylist", help="titre choisis")
-parser.add_argument("--artistePlaylist", help="nom de l'artiste")
-parser.add_argument("--albumPlaylist", help="album présent dans la playlist")
-parser.add_argument("--genrePlaylist", help="genre%pourcentage", nargs=2)
+parser.add_argument("-t", "--titrePlaylist", help="titre choisis")
+parser.add_argument("-ar", "--artistePlaylist", help="nom de l'artiste")
+parser.add_argument("-al", "--albumPlaylist", help="album présent dans la playlist")
+parser.add_argument("-g", "--genrePlaylist", help="genre%pourcentage", nargs=2)
 
 args = parser.parse_args()
 
-def verifier_mes_quantité(quantite):
-    return int(quantite)
+# On affiche les arguments obligatoire
+logging.info(args.dureePlaylist)
+logging.info(args.formatPlaylist)
+logging.info(args.nomFichierPlaylist)
+
+def verifier_mes_quantite(quantite):
+    try:
+        logging.info("Mise en marche de la fonction")
+        genre = abs(int(quantite))
+        if 0 < genre > 100:
+            raise Exception('Erreur')
+        return genre
+    except ValueError:
+        logging.error("La valeur saisie pour la quantité n'est pas une valeur numérique : '" + quantite + "'")
+        exit(1)
+    except Exception as err:
+        if err.args[0] == 'Erreur':
+            logging.error("La valeur saisie pour la quantité ne peut être négative ou supérieur à 100: '%i'" % genre)
+            exit(1)
+
+#print(args)
+args.genrePlaylist[1] = verifier_mes_quantite(args.genrePlaylist[1])
 
 
 
-try:
-    args.genrePlaylist[1] = verifier_mes_quantité(args.genrePlaylist[1])
-    print(args)
-except ValueError:
-    print ("Erreur lors de la conversion !")
+for argument in ['titrePlaylist','artistePlaylist','albumPlaylist','genrePlaylist']:
+# Si l'argument est renseigné
+    if getattr(args, argument) is not None:
+        # On écrit la valeur de ses ss-arg dans le fichier de logs
+        logging.info(' Argument --' + argument + ' :\t' + getattr(args, argument)[0] + ' ; ' + str(getattr(args, argument)[1]))
+verifier_mes_quantite(getattr(args, argument))
 
 
-
-if args.genrePlaylist[1] > 0 and args.genrePlaylist[1] <= 100:
-    print ("ok")
-else:
-    print("Veuillez saisir un nombre entre 0 et 100")
-
+logging.debug(' *****************************************')
+logging.shutdown()
+exit(0)
